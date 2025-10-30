1b6a0d34a1ea6e7f65e53cd6107f5ab20d5e7fe7
Marce Esquinas <228049936+MarceEsquinas@users.noreply.github.com>
2025-10-30 09:50:29 +0100

    creo boton

---
+++ src/components/buton.jsx
@@ -0,0 +1,7 @@
+export const Buton = () =>{
+  return (
+    <button className="px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
+      ¡Haz clic!
+    </button>
+  );
+}
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,14 +1,17 @@
 import { LandingContent } from "../components/landingContent"
 import { Footer } from "../components/Footer"
 import { Header } from "../components/header"
-
+import { Buton } from "../components/buton"
 
 export const Landing = () => {
     return (
         <div className="flex flex-col h-screen w-full">
             <Header />
             <LandingContent />
+            <LandingContent />
             <Footer />
+            <Buton/>
+        
         </div>
     )
 }
