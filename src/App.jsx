e91097f9f0fddfecd9b027af4e59eff409372071
Marce Esquinas <228049936+MarceEsquinas@users.noreply.github.com>
2025-11-04 17:06:42 +0100

    modifico elemento Playlist y añado ruta song

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { Landing } from './pages/landing'
 import { Login } from './pages/login'
+import { Playlist } from './pages/playlist'
+import { Songs } from './pages/songs'
 import {Routes, Route} from "react-router-dom"
 
 function App() {
@@ -9,7 +11,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Landing/>} />
       <Route path="/login" element={<Login/>} />
-      <Route path="/playlist" element={<Landing/>} />
+      <Route path="/playlist" element={<Playlist/>} />
+      <Route path="/songs" element={<Songs/>} />
     </Routes>
   )
 }
